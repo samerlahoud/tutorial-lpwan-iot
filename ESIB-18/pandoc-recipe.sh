387fa0004614f1df760eb8ae8a2775d28814d823
samerlahoud <samer@lahoud.fr>
2018-06-18 23:47:58 +0300

    experimentation v0

--- ESIB-18/pandoc-recipe.sh
+++ ESIB-18/pandoc-recipe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pandoc --latex-engine=xelatex -t beamer front-matter.md general-framework.md design-rationales.md technical-specs.md perf-evaluation.md experimentation.md -H ./generation-script/in_header.txt -o lpwan-iot-tutorial-esib-18.pdf
+#pandoc --latex-engine=xelatex -t beamer front-matter.md general-framework.md design-rationales.md technical-specs.md perf-evaluation.md experimentation.md -H ./generation-script/in_header.txt -o lpwan-iot-tutorial-esib-18.pdf
 #compilation can be partial
-#pandoc --latex-engine=xelatex -t beamer front-matter.md technical-specs.md -H ./generation-script/in_header.txt -o lpwan-iot-tutorial-esib-18.pdf
+pandoc --latex-engine=xelatex -t beamer front-matter.md experimentation.md -H ./generation-script/in_header.txt -o lpwan-iot-tutorial-esib-18.pdf
 echo "magic is done!"
