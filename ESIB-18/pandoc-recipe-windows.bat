794d4eedb3f4e5bf5038cf70e2dd9f379aedb770
melhem1986 <melhem.helou@usj.edu.lb>
2018-06-19 00:12:50 +0300

    link-budget
    v0

--- ESIB-18/pandoc-recipe-windows.bat
+++ ESIB-18/pandoc-recipe-windows.bat
@@ -2,4 +2,5 @@
 
 @REM compilation can be partial
 pandoc --latex-engine=xelatex -t beamer front-matter.md design-rationales.md technical-specs.md -H ./generation-script/in_header.txt -o lpwan-iot-tutorial-esib-18.pdf
+
 echo "magic is done!"
