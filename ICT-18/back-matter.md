9b9fc46a527bbfdf7009fde23fba7933d8e503b3
melhem1986 <melhem.helou@usj.edu.lb>
2018-06-27 03:40:50 +0300

    minor

--- ICT-18/back-matter.md
+++ ICT-18/back-matter.md
@@ -1,5 +1,5 @@
 ### Feedback and Tutorial Material
-- You feedback is important to us in order to continually improve our tutorial
+- Your feedback is important to us in order to continually improve our tutorial
 - We appreciate if you could take five minutes to complete the following evaluation form: https://goo.gl/Ex7mg9
 - We made the tutorial sources available under Creative Common license CC BY-NC-SA 4.0
     - This is a human-readable summary of the license: https://creativecommons.org/licenses/by-nc-sa/4.0/
--- ICT-18/pandoc-recipe-windows.bat
+++ ICT-18/pandoc-recipe-windows.bat
@@ -1,4 +1,4 @@
-@REM pandoc --latex-engine=xelatex -t beamer front-matter.md general-framework.md design-rationales.md technical-specs.md perf-evaluation.md research-challenges.md -H ./generation-script/in_header.txt -o lpwan-iot-tutorial-esib-18.pdf
+@REM pandoc --latex-engine=xelatex -t beamer front-matter.md general-framework.md design-rationales.md technical-specs.md perf-evaluation.md research-challenges.md back-matter.md -H ./generation-script/in_header.txt -o lpwan-iot-tutorial-ict-18.pdf
 
 @REM compilation can be partial
 pandoc --latex-engine=xelatex -t beamer front-matter.md technical-specs.md -H ./generation-script/in_header.txt -o lpwan-iot-tutorial-ict-18.pdf
