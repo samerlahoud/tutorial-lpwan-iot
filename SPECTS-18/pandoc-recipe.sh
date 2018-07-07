e28881625b74d4e598a714713c07b213beb59d35
samerlahoud <samer@lahoud.fr>
2018-07-08 00:29:38 +0200

    tidy folder

--- SPECTS-18/pandoc-recipe.sh
+++ SPECTS-18/pandoc-recipe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #pandoc --pdf-engine=xelatex -f markdown+raw_tex -t beamer front-matter.md general-framework.md design-rationales.md technical-specs.md perf-evaluation.md research-challenges.md back-matter.md -H ./generation-script/in_header.txt -o /Users/simurgh/Desktop/lpwan-iot-tutorial-spects-18.pdf
 #compilation can be partial
-pandoc --pdf-engine=xelatex -f markdown+raw_tex -t beamer front-matter.md general-framework.md technical-specs.md perf-evaluation.md back-matter.md -H ./generation-script/in_header.txt -o /Users/simurgh/Desktop/lpwan-iot-tutorial-ict-18.pdf
+pandoc --pdf-engine=xelatex -f markdown+raw_tex -t beamer front-matter.md general-framework.md technical-specs.md perf-evaluation.md back-matter.md -H ./generation-script/in_header.txt -o /Users/simurgh/Desktop/lpwan-iot-tutorial-spects-18.pdf
 #pandoc -s -t beamer -f markdown+raw_tex front-matter.md general-framework.md design-rationales.md technical-specs.md perf-evaluation.md research-challenges.md back-matter.md -H ./generation-script/in_header.txt -o /Users/simurgh/Desktop/lpwan-iot-tutorial.tex
 echo "magic is done!"
